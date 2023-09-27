7505241b52c552f58bb3e8acfc3577505a5ff4e2
Joseph Hoang <jhoang949@gmail.com>
2023-09-27 01:49:40 -0700

    -Added some comments to existing files -Created a new file logic.py that would deal with game logic -New functions to:  1) take user input and call another function to perform algorithm  2) perform algorithms based on input  *NOTE  Only the skeleton outline for the algorithm functions were made.  Implementations are not yet coded

---
+++ logic.py
@@ -0,0 +1,37 @@
+
+
+
+
+# Function to read user intake and call subsequent functions
+# to perform actions on the board, depending on user response
+def user_resp(char, board):
+    if char == 'a':
+        left_alg(board)
+    elif char == 'd':
+        right_alg(board)
+    elif char == 'w':
+        up_alg(board)
+    elif char == 's':
+        down_alg(board)
+
+
+# Functions to perform shifts on the game board, depending on user input:
+
+# Function to perform left shift on the board
+def left_alg(board):
+    return board
+
+
+# Function to perform left shift on the board
+def right_alg(board):
+    return board
+
+
+# Function to perform left shift on the board
+def up_alg(board):
+    return board
+
+
+# Function to perform left shift on the board
+def down_alg(board):
+    return board
